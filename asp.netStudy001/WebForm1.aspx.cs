24a4858816391c92227a4dabffab7f6dd7e757ce
KIM SANG KI <ckacl0233@gmail.com>
2022-07-20 17:25:38 +0900

    web Form Test / Master Page Layout

---
+++ asp.netStudy001/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp.netStudy001
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string msg = "";
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            msg = "<script type='text/javascript'>alert('WebFormInit')</script>";
+            //Response.Write(msg);
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            msg = "<script type='text/javascript'>alert('WebFormLoad')</script>";
+            //Response.Write(msg);
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            msg = "<script type='text/javascript'>alert('WebFormPreRender')</script>";
+            //Response.Write(msg);
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            msg = "<script type='text/javascript'>alert('어쩌고버튼')</script>";
+            Response.Write(msg);
+        }
+    }
+}
---
+++ asp.netStudy001/WebFormTest002.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace asp.netStudy001
+{
+    public partial class WebFormTest002 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
